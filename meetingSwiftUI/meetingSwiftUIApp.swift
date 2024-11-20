25d63691253f631a6365f643962a21eef712350d
Juliano Almada <julianoalmada48@gmail.com>
2024-11-20 00:14:57 -0300

    Initial Commit

---
+++ meetingSwiftUI/meetingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  meetingSwiftUIApp.swift
+//  meetingSwiftUI
+//
+//  Created by Juliano Almada on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct meetingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
